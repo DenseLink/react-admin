10de99ae6b8527cdf89c3996bd91375151524650
DenseLink <tardis1103@gmail.com>
2023-02-03 15:57:58 -0500

    create window manager

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,58 @@
 import dynamic from 'next/dynamic';
-import type { ReactElement } from 'react';
+import type { FC, ReactElement} from 'react';
+// eslint-disable-next-line import/no-duplicates
+import { createContext} from 'react';
+// eslint-disable-next-line import/no-duplicates
+import { useState } from 'react';
 
 
+type Process = {
+  Component: any;
+};
 
-const processDirectory = {
+type Processes = {
+  [id: string]: Process;
+};
+
+type ProcessContextState = {
+  processes: Partial<Process>;
+};
+
+const ProcessContext = createContext<ProcessContextState>({ processes: {} });
+
+const processDirectory: Processes = {
   HelloWorld: {
     // eslint-disable-next-line import/extensions
     Component: dynamic(() => import('./HelloWorld'))
   }
 };
 
+const ProcessProvider: FC = ({ children }) => {
+  const [processes] = useState(processDirectory);
 
+  return (
+    <ProcessContext.Provider value={{ processes }}>
+      {children}
+    </ProcessContext.Provider>
+  );
+};
+
+const ProcessConsumer = ProcessContext.Consumer;
+
+const WindowManager: FC = () => (
+  <ProcessConsumer>
+    {({ processes }) =>
+      Object.entries(processes).map(([id, { Component }]) => (
+        <Component key={id} />
+      ))
+    }
+  </ProcessConsumer>
+);
 
 export default function Home(): ReactElement {
-  const HelloWorldComponent = processDirectory.HelloWorld.Component;
-  return <HelloWorldComponent />;
+  return (
+    <ProcessProvider>
+      <WindowManager />
+    </ProcessProvider>
+  );
 }
