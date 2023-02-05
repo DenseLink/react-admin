a90d70fab0147246be832435421248686ba09366
DenseLink <tardis1103@gmail.com>
2023-02-05 14:53:48 -0500

    shorten document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,5 @@
 import type { DocumentContext, DocumentInitialProps } from 'next/document';
-import Document, { Head, Html, Main, NextScript } from 'next/document';
-import type { ReactElement } from 'react';
+import Document from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 const withServerStyleSheet = async (
@@ -40,17 +39,6 @@ class MyDocument extends Document {
     return withServerStyleSheet(ctx);
   }
 
-  render(): ReactElement {
-    return (
-      <Html lang="en">
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
 }
 
 export default MyDocument;
