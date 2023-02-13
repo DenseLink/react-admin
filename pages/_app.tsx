7e498e90285cd82fd348888133b614d3eae1f3cf
DenseLink <tardis1103@gmail.com>
2023-02-13 18:22:59 -0500

    Don't allow user select

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -24,6 +24,7 @@ const GlobalStyle = createGlobalStyle`
   }
   body {
     font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    user-select: none;
   }
   button {
     background-color: transparent;
