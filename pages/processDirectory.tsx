dbbcf2a54bd3cf774e3cfb93ce1735349c2cb94c
DenseLink <tardis1103@gmail.com>
2023-02-13 20:08:21 -0500

    moved favicon

--- pages/processDirectory.tsx
+++ pages/processDirectory.tsx
@@ -7,7 +7,7 @@ const processDirectory: Processes = {
     // eslint-disable-next-line import/extensions
     Component: dynamic(() => import('./HelloWorld')),
     hasWindow: true,
-    icon: './favicon.ico',
+    icon: '../favicon.ico',
     title: 'Hello World'
   }
 };
