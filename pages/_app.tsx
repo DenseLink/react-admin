afdf618f613870b9c1606922fbf84418012f1740
DenseLink <tardis1103@gmail.com>
2023-02-14 18:57:45 -0500

    Allow Prop Spreading

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -118,7 +118,6 @@ const App = ({ Component, pageProps }: AppProps): React.ReactElement => (
           description={packageJson.name}
           title={packageJson.description}
         />
-        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
         <Component {...pageProps} />
       </StyledApp>
     </SessionProvider>
