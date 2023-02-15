cf91bf7e1cf768c40a0623db84d5db35a50b2fc4
DenseLink <tardis1103@gmail.com>
2023-02-14 19:41:31 -0500

    Custom double click handler

--- pages/FileManager/FileEntry.tsx
+++ pages/FileManager/FileEntry.tsx
@@ -1,4 +1,4 @@
-import { useCallback } from 'react';
+import { useCallback, useRef } from 'react';
 import styled from 'styled-components';
 
 // eslint-disable-next-line import/extensions
@@ -20,14 +20,39 @@ const Button = styled.button.attrs({
   font-family: inherit;
 `;
 
+type DoubleClick = (
+  handler: React.MouseEventHandler,
+  timeout?: number
+) => React.MouseEventHandler;
+
+const useDoubleClick: DoubleClick = (handler, timeout = 500) => {
+  const timer = useRef<NodeJS.Timeout | null>(null);
+  const onClick = useCallback<React.MouseEventHandler>(
+    (event) => {
+      if (!timer.current) {
+        timer.current = setTimeout(() => {
+          timer.current = null;
+        }, timeout);
+      } else {
+        clearTimeout(timer.current);
+        handler(event);
+        timer.current = null;
+      }
+    },
+    [handler, timeout]
+  );
+
+  return onClick;
+};
+
 const FileEntry = ({ name, path }: FileEntryProps): JSX.Element => {
   const { icon, pid } = useFileInfo(path);
   const { open } = useProcesses();
-  const onActivate = useCallback(() => open(pid), [open, pid]);
+  const onClick = useCallback(() => open(pid), [open, pid]);
 
   return (
     <StyledFileEntry>
-      <Button onClick={onActivate} onKeyDown={onActivate}>
+      <Button onClick={useDoubleClick(onClick)}>
         <figure>
           <img src={icon} alt={name} />
           <figcaption>{name}</figcaption>
