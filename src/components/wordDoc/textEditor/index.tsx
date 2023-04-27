bae906bf7b29eb1a3709cb0e213a538afc619763
DenseLink <tardis1103@gmail.com>
2023-04-27 13:20:25 -0400

    fixed quill styling

--- src/components/wordDoc/textEditor/index.tsx
+++ src/components/wordDoc/textEditor/index.tsx
@@ -2,7 +2,8 @@ import "quill/dist/quill.snow.css";
 
 import dynamic from "next/dynamic";
 import { useCallback, useState } from "react";
-import styled from "styled-components";
+
+import { TextEditorWrapper } from "./textEditorStyles";
 
 const ReactQuill = dynamic(import("react-quill"), {
   loading: () => <p>Loading ...</p>,
@@ -12,7 +13,7 @@ const ReactQuill = dynamic(import("react-quill"), {
 const TextEditor = (): JSX.Element => {
   const [value, setValue] = useState("");
 
-  const handleChange = useCallback((newValue) => {
+  const handleChange = useCallback((newValue: string) => {
     setValue(newValue);
   }, []);
 
@@ -22,21 +23,5 @@ const TextEditor = (): JSX.Element => {
     </TextEditorWrapper>
   );
 };
-
-const TextEditorWrapper = styled.div`
-  .quill {
-    background-color: #f3f3f3;
-    margin: 0;
-  }
-
-  .ql-container {
-    width: 2.5in;
-    min-height: 11in;
-    padding: 11in;
-    margin: 1rem;
-    box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.5);
-    background-color: white;
-  }
-`;
 
 export default TextEditor;
--- src/components/wordDoc/textEditor/textEditorStyles.ts
+++ src/components/wordDoc/textEditor/textEditorStyles.ts
@@ -1,11 +1,76 @@
 import styled from "styled-components";
 
-export const TextBody = styled.div`
-  background-color: #f3f3f3;
-  margin: 0;
-  width: 8.5in;
-  min-height: 11in;
-  padding: 1in;
-  margin: 1rem;
-  box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.5);
+export const TextEditorWrapper = styled.div`
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+  }
+  body {
+    background-color: #49c01a;
+    margin: 0;
+  }
+  .container .ql-editor {
+    width: 8.5in;
+    min-height: 11in;
+    padding: 1in;
+    margin: 1rem;
+    box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.5);
+    background-color: #c51111ff;
+  }
+
+  .quill {
+    background-color: white;
+    margin: 0;
+  }
+
+  .ql-container {
+    width: 8.5in;
+    min-height: 11in;
+    padding: 1in;
+    margin: 1rem;
+    box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.5);
+  }
+  .container .ql-container.ql-snow {
+    border: none;
+    display: flex;
+    justify-content: center;
+    background-color: #dd2525;
+  }
+
+  .container .ql-toolbar.ql-snow {
+    display: flex;
+    justify-content: center;
+    position: sticky;
+    top: 0;
+    z-index: 1;
+    background-color: #d11e1e;
+    border: none;
+    box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.5);
+    width: 8.5in;
+  }
+
+  @page {
+    margin: 2in;
+  }
+
+  @media print {
+    body {
+      background: none;
+    }
+
+    .container .ql-editor {
+      width: 6.5in;
+      height: 9in;
+      padding: 0;
+      margin: 0;
+      box-shadow: none;
+      align-self: flex-start;
+    }
+
+    .container .ql-toolbar.ql-snow {
+      display: none;
+      width: 8.5in;
+    }
+  }
 `;
